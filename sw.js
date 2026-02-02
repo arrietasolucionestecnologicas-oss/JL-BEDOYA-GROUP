bfc46a9e42b488903da483defdac09b3dab2365a
arrietasolucionestecnologicas-oss <arrietasolucionestecnologicas@gmail.com>
2026-02-02 14:18:56 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
-// sw.js - Service Worker Mínimo para PWA
+// sw.js - Service Worker
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+  console.log('[Service Worker] Instalado correctamente');
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', (e) => {
+  console.log('[Service Worker] Activado');
+  return self.clients.claim();
+});
+
 self.addEventListener('fetch', (e) => {
-  // Necesario para que sea instalable
+  // Respondemos siempre con la red para evitar datos viejos
+  e.respondWith(fetch(e.request));
 });
